dcbb6c48aeb57e3a7809ea9500a384c8647c16d6
OrangeCocoa <shoheyhey1111@yahoo.co.jp>
2019-11-15 10:46:44 +0900

    steamVRからarrow, target移植中

---
+++ Assets/Resources/Bow/Scripts/ArrowheadRotation.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class ArrowheadRotation : MonoBehaviour
+{
+    void Start()
+    {
+        float randX = Random.Range(0f, 180f);
+        transform.localEulerAngles = new Vector3(randX, -90f, 90f);
+    }
+}
---
+++ Assets/Source/TargetCollision.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetCollision : MonoBehaviour
+{
+    [SerializeField] GameObject target;
+
+    void Awake()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        GameObject collisionObj = other.gameObject;
+        if (!collisionObj.CompareTag("Arrow")) return;
+
+        // ToDo : ArrowをTargetの子オブジェクトにして現在位置に固定
+    }
+}
