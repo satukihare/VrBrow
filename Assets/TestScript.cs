e3f33dcc84a587e03912a8e48895adaa9cd9f09e
starUchan <wataru@hiromatsu.net>
2019-11-21 13:03:10 +0900

    サウンドテスト用のTestScriptを作成　

--- Assets/TestScript.cs
+++ Assets/TestScript.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class TestScript : MonoBehaviour
 {
-    public bool enable;
-    public AudioClip sound;
+    public bool enable = true;
+    public AudioClip sound = null;
+    public float loopTime = 0.1;
 
     private AudioSource audioSource;
+    private float time;
+    private bool timerCheck;
 
     // Start is called before the first frame update
     void Start()
@@ -19,14 +22,32 @@ public class TestScript : MonoBehaviour
         }
 
         audioSource.clip = sound;
+        time = 0;
+        timerCheck = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(enable)
+        if(enable && audioSource.clip != null && timerCheck)
         {
             audioSource.Play();
+        }
+
+        TimeManager();
+    }
+
+    void TimeManager()
+    {
+        if(time < loopTime)
+        {
+            timerCheck = false;
+            time += Time.deltaTime;
+        }
+        else
+        {
+            timerCheck = true;
+            time = 0;
         }
     }
 }
