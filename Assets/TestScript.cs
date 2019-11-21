efdd41343bcc031e14ec047dd66b473c61e28d9e
starUchan <wataru@hiromatsu.net>
2019-11-21 12:51:11 +0900

    効果音を追加

---
+++ Assets/TestScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+    public bool enable;
+    public AudioClip sound;
+
+    private AudioSource audioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        if (audioSource == null)
+        {
+            audioSource = gameObject.AddComponent<AudioSource>();
+        }
+
+        audioSource.clip = sound;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(enable)
+        {
+            audioSource.Play();
+        }
+    }
+}
