0db1ba934bd760a9ecd3576a7d0d7b570e6190e5
suneo2612 <suneo2612@users.noreply.github.com>
2019-12-12 10:17:28 +0900

    タイトル処理です

---
+++ Assets/Seita/Title_Manager.cs
@@ -0,0 +1,144 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// タイトルマネージャー
+public class Title_Manager : MonoBehaviour
+{
+    //=====================================================================
+    //                             メンバ変数
+    //=====================================================================
+    // 外部入力値
+    [SerializeField]
+    private GameObject      m_pTitleRogoObject;             // タイトルロゴ
+    private Title_Scaling   m_pTitleScalingComponent;       // タイトルロゴサイズ変化用コンポーネント
+    [SerializeField]
+    private GameObject      m_pCheckButtonRogoObject;       // ボタン確認ロゴ
+    private Title_Fade      m_pCheckButtonFadeComponent;    // ボタン確認ロゴサイズ変化用コンポーネント
+
+    // 変数
+    private bool            m_bTitleSwitch;                 // タイトル表示スイッチ
+    private bool            m_bTitleShow;                   // タイトルが出きったかフラグ
+    private bool            m_bTitleHide;                   // タイトルが消え切ったかフラグ
+
+
+    //=====================================================================
+    //                             メンバ関数
+    //=====================================================================
+    // タイトルアニメーション制御
+    public void TitleFlag(bool bTitleSwitchFlag)
+    {
+        // 表示
+        if (m_pCheckButtonFadeComponent.GetSizeOut() && m_pTitleScalingComponent.GetSizeOut())
+        {
+            m_bTitleSwitch = bTitleSwitchFlag;
+        }
+
+        // 非表示
+        if (m_pCheckButtonFadeComponent.GetSizeIn() && m_pTitleScalingComponent.GetSizeIn())
+        {
+            m_bTitleSwitch = bTitleSwitchFlag;
+        }
+    }
+
+    // タイトルが出きったか渡す
+    public bool TitleShowFlag()
+    {
+        return m_bTitleShow;
+    }
+
+    // タイトルが消え切ったか渡す
+    public bool TitleHideFlag()
+    {
+        return m_bTitleHide;
+    }
+
+    // タイトルが出きったか消え切ったかフラグを切り替えていく
+    private void HideShowCheck()
+    {
+        // 両フラグを初期化
+        m_bTitleHide = m_bTitleShow = false;
+
+        // 非表示
+        if(m_pCheckButtonFadeComponent.GetSizeOut() && m_pTitleScalingComponent.GetSizeOut() && !m_pCheckButtonFadeComponent.GetSizeIn() && !m_pTitleScalingComponent.GetSizeIn())
+        {
+            m_bTitleHide = true;
+        }
+
+        // 表示
+        if (!m_pCheckButtonFadeComponent.GetSizeOut() && !m_pTitleScalingComponent.GetSizeOut() && m_pCheckButtonFadeComponent.GetSizeIn() && m_pTitleScalingComponent.GetSizeIn())
+        {
+            m_bTitleShow = true;
+        }
+    }
+
+    // タイトルアニメーション
+    private void TitleAnimation()
+    {
+        // 表示
+        if (m_bTitleSwitch)
+        {
+            // タイトルを表示
+            m_pTitleScalingComponent.ChangeFadeSwitch(true);
+
+            // もしタイトルが表示しきったら
+            if (m_pTitleScalingComponent.GetSizeIn())
+            {
+                m_pCheckButtonFadeComponent.ChangeFadeSwitch(false);
+            }
+        }
+        // 非表示
+        else
+        {
+            // タイトルを表示
+            m_pCheckButtonFadeComponent.ChangeFadeSwitch(true);
+
+            // もしタイトルが消えたら
+            if (m_pCheckButtonFadeComponent.GetSizeOut())
+            {
+                m_pTitleScalingComponent.ChangeFadeSwitch(false);
+            }
+        }
+    }
+
+
+    //=====================================================================
+    //                              基本関数
+    //=====================================================================
+    // 初期化
+    // Start is called before the first frame update
+    void Start()
+    {
+        // コンポーネント取得
+        m_pTitleScalingComponent = m_pTitleRogoObject.GetComponent<Title_Scaling>();
+        m_pCheckButtonFadeComponent = m_pCheckButtonRogoObject.GetComponent<Title_Fade>();
+
+        // 変数初期化
+        m_bTitleSwitch = true;
+        m_bTitleShow = m_bTitleHide = false;
+    }
+
+    // 更新
+    // Update is called once per frame
+    void Update()
+    {
+        // テスト
+        if(Input.GetKeyDown(KeyCode.Return))
+        {
+            if(m_bTitleSwitch)
+            {
+                TitleFlag(false);
+            }
+            else
+            {
+                TitleFlag(true);
+            }
+        }
+
+        // タイトルアニメーション
+        TitleAnimation();
+
+        // タイトルの状態確認
+        HideShowCheck();
+    }
+}
---
+++ Assets/Seita/Title_Scaling.cs
@@ -0,0 +1,126 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Title_Scaling : MonoBehaviour
+{
+    //=====================================================================
+    //                             メンバ変数
+    //=====================================================================
+    // 外部入力値
+    [SerializeField, Range(0.0f, 1.0f)]
+    private float           m_fSizeChangeSpeed;             // フェード速度
+
+    // 変数
+    private TextMeshPro     m_pTextMeshProComponent;        // テキストメッシュプロコンポーネント
+    private float           m_fDefaultFontSize;             // 通常のフォントサイズ
+    private float           m_fFontSize;                    // フォントサイズ
+    private float           m_fChangeSizeSpeed;             // サイズ切り替え速度
+    private bool            m_bSizeOut;                     // フォントが最小
+    private bool            m_bSizeIn;                      // フォントが最大
+    private bool            m_bSizeSwitch;                  // フェードスイッチ(縮小 が False, 拡大 が true)
+
+
+    //=====================================================================
+    //                             メンバ関数
+    //=====================================================================
+    // サイズアウトしたか状態を渡す
+    public bool GetSizeOut()
+    {
+        return m_bSizeOut;
+    }
+
+    // サイズインしたか状態を渡す
+    public bool GetSizeIn()
+    {
+        return m_bSizeIn;
+    }
+
+    // サイズ変化スイッチ(true で 拡大, false で縮小)
+    public void ChangeFadeSwitch(bool bFadeSwitch)
+    {
+        // 切り替え時のみ更新
+        if (m_bSizeSwitch != bFadeSwitch)
+        {
+            m_bSizeSwitch = bFadeSwitch;
+        }
+    }
+
+    // フェード値変化
+    private void ChangeFontSize()
+    {
+        // フォントが消えたかフラグ と 出たかフラグ を初期化
+        m_bSizeOut = false;
+        m_bSizeIn = false;
+
+        // スイッチで切り替え
+        // 縮小
+        if (!m_bSizeSwitch)
+        {
+            // フォントサイズ縮小
+            m_fFontSize -= m_fChangeSizeSpeed;
+
+            // 制限
+            if (m_fFontSize < 0.0f)
+            {
+                m_fFontSize = 0.0f;
+
+                // 完全にフォントが消えたことを伝える
+                m_bSizeOut = true;
+            }
+        }
+        // 拡大
+        else
+        {
+            // フォントサイズ拡大
+            m_fFontSize += m_fChangeSizeSpeed;
+
+            // 制限
+            if (m_fFontSize > m_fDefaultFontSize)
+            {
+                m_fFontSize = m_fDefaultFontSize;
+
+                // 完全にフォントが大きくなったことを伝える
+                m_bSizeIn = true;
+            }
+        }
+    }
+
+    // テキストフェード
+    private void ChangeSIzeFunction()
+    {
+        // 色更新
+        m_pTextMeshProComponent.fontSize = m_fFontSize;
+    }
+
+
+    //=====================================================================
+    //                              基本関数
+    //=====================================================================
+    // 初期化
+    // Start is called before the first frame update
+    void Start()
+    {
+        // コンポーネント取得
+        m_pTextMeshProComponent = GetComponent<TextMeshPro>();
+
+        // テキスト拡縮速度確定処理
+        m_fDefaultFontSize = m_fFontSize = m_pTextMeshProComponent.fontSize;
+        m_fChangeSizeSpeed = m_fDefaultFontSize * m_fSizeChangeSpeed;
+
+        // 変数初期化
+        m_bSizeSwitch = true;
+    }
+
+    // 更新
+    // Update is called once per frame
+    void Update()
+    {
+        // サイズ切り替え処理
+        ChangeFontSize();
+
+        // サイズ変更処理
+        ChangeSIzeFunction();
+    }
+}
